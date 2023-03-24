201785b1708b0103dc413bfd37db5816672ab8a2
majapiszczek <piszczekmaja33@gmail.com>
2023-03-24 15:59:24 +0100

    added css

---
+++ script.js
@@ -0,0 +1,4 @@
+const imgList = document.querySelector(".gallery-slide-list");
+const images = Array.from(imgList.children);
+const nextImg = document.querySelector(".next");
+const prevImg = document.querySelector(".previous");
